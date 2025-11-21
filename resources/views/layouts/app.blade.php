e6aa12f7a1bcf4ad74a2ee8d1fa314b376d7a018
Gabriel Felipe <62359678+xiuris@users.noreply.github.com>
2025-11-20 23:35:52 -0400

    chore: wire homstech os theme assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="{{ asset('css/homstech-os.css') }}">
+    @if (app()->environment('testing'))
+        <link rel="stylesheet" href="{{ asset('css/homstech-os.css') }}">
+    @else
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
 </head>
 <body>
 <div class="app-shell">
